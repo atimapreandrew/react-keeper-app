572ba7fa81f33b59778fd641b13a3cf909f10656
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-12 14:24:53 +0100

    Added Dexiejs(IndexedDB) for Storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,33 +3,40 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { useLiveQuery } from "dexie-react-hooks";
+import { db } from "../dexie";
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const notes = useLiveQuery(() => db.notes.toArray());
+  // const [notes, setNotes] = useState([]);
 
-  const addNote = (note) => {
-    setNotes((prevNotes) => {
-      return [...prevNotes, note];
-    });
+  const addNote = async (note) => {
+    await db.notes.add(note);
+
+    // setNotes((prevNotes) => {
+    //   return [...prevNotes, note];
+    // });
   };
 
-  const deleteNote = (id) => {
-    setNotes((prevNotes) => {
-      return prevNotes.filter((note, index) => {
-        return index !== id;
-      });
-    });
+  const deleteNote = async (id) => {
+    await db.notes.delete(id);
+
+    // setNotes((prevNotes) => {
+    //   return prevNotes.filter((note, index) => {
+    //     return index !== id;
+    //   });
+    // });
   };
 
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((note, index) => {
+      {notes?.map((note, index) => {
         return (
           <Note
             key={index}
-            id={index}
+            id={note.id}
             title={note.title}
             content={note.content}
             onDelete={deleteNote}
